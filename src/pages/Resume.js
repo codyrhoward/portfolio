424faaffcec0d6ade8611730a697f31117db83dc
codyrhoward <codyrhoward19@gmail.com>
2020-10-04 20:17:23 -0400

    react side-bar with router

---
+++ src/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <div className="search">
+            <h1>hello from search</h1>
+        </div>
+    )
+}
+
+export default Search
+
